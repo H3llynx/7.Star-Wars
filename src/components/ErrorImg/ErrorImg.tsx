56e3fef2c6e631e768b1fabf93014596e8418d83
Helene T <helene.teychene@gmail.com>
2026-01-26 16:10:32 +0100

    refactor / minor styling improvements

---
+++ src/components/ErrorImg/ErrorImg.tsx
@@ -0,0 +1,11 @@
+import "./ErrorImg.css"
+
+export function ErrorImg() {
+    return (
+        <img
+            src="jarjar.png"
+            className="error-img"
+            alt="Error: data not found"
+        />
+    )
+}
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,6 +1,8 @@
+import "./Loading.css"
+
 export function Loading() {
     return (
-        <p className="scan-terminal">
+        <p className="scan-terminal loading">
             <span className="terminal-prompt">&gt;_</span>
             Loading
             <span className="terminal-cursor">|</span>
