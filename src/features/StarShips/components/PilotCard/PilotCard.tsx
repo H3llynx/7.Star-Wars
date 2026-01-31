5779644c82e11b3932ad0cc447c9c03c56890392
Helene T <helene.teychene@gmail.com>
2026-01-31 17:13:41 +0100

    feature / finished pilot card styling

--- src/features/StarShips/components/PilotCard/PilotCard.tsx
+++ src/features/StarShips/components/PilotCard/PilotCard.tsx
@@ -5,7 +5,10 @@ export function PilotCard({ pilot }: { pilot: Pilot }) {
     return (
         <div className="pilot-card">
             <div className="pilot-avatar">
-                <img src={pilot.src} alt={""} />
+                <img src={pilot.src} alt={""}
+                    onError={(e) => {
+                        (e.currentTarget as HTMLImageElement).src = "jarjar.png";
+                    }} />
             </div>
             <p className="pilot-name">{pilot.name}</p>
         </div>
--- src/features/StarShips/components/ShipView/ShipView.stories.tsx
+++ src/features/StarShips/components/ShipView/ShipView.stories.tsx
@@ -12,8 +12,7 @@ const meta: Meta<typeof ShipView> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const ship =
-{
+const ship = {
     "name": "Death Star",
     "model": "DS-1 Orbital Battle Station",
     "manufacturer": "Imperial Department of Military Research, Sienar Fleet Systems",
@@ -38,11 +37,69 @@ const ship =
     src: "starships/9.webp"
 }
 
+const shipWithPilots = {
+    "name": "Millennium Falcon",
+    "model": "YT-1300 light freighter",
+    "manufacturer": "Corellian Engineering Corporation",
+    "cost_in_credits": "100000",
+    "length": "34.37",
+    "max_atmosphering_speed": "1050",
+    "crew": "4",
+    "passengers": "6",
+    "cargo_capacity": "100000",
+    "consumables": "2 months",
+    "hyperdrive_rating": "0.5",
+    "MGLT": "75",
+    "starship_class": "Light freighter",
+    "films": [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/"
+    ],
+    "created": "2014-12-10T16:59:45.094000Z",
+    "edited": "2014-12-20T21:23:49.880000Z",
+    "url": "https://swapi.dev/api/starships/10/",
+    id: "10",
+    src: "starships/10.webp",
+    pilots: [
+        {
+            id: "13",
+            name: "Chewbacca",
+            src: "pilots/13.webp"
+        },
+        {
+            id: "14",
+            name: "Han Solo",
+            src: "pilots/14.webp"
+        }
+    ],
+}
+
 export const Default: Story = {
     render: () => {
         store.dispatch({
             type: 'starships/fetchShipById/fulfilled',
             payload: ship
+        });
+        return <ShipView />;
+    },
+    parameters: {
+        reactRouter: reactRouterParameters({
+            location: {
+                pathParams: { id: ship.id }
+            },
+            routing: {
+                path: 'starships/:id'
+            },
+        }),
+    },
+};
+
+export const DefaultWithPilots: Story = {
+    render: () => {
+        store.dispatch({
+            type: 'starships/fetchShipById/fulfilled',
+            payload: shipWithPilots
         });
         return <ShipView />;
     },
