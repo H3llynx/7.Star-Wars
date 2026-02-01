2c9067d998086a143ed629e204d0731215bdf694
Helene T <helene.teychene@gmail.com>
2026-02-01 10:31:04 +0100

    refactor / added loading text and  animation inside cta and authentication area + minor style adjustments

--- src/components/AuthArea/AuthArea.tsx
+++ src/components/AuthArea/AuthArea.tsx
@@ -3,7 +3,7 @@ import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { logoutUser } from "../../features/Auth/AuthSlice";
 
 export function AuthArea() {
-    const { user } = useAppSelector(state => state.auth);
+    const { user, loading } = useAppSelector(state => state.auth);
     const dispatch = useAppDispatch();
 
     const handleLogOut = async () => {
@@ -12,7 +12,8 @@ export function AuthArea() {
 
     return (
         <div className="auth-area">
-            {user &&
+            {loading && <span className="text-loading">Loading</span>}
+            {user && !loading &&
                 <>
                     <span>Operator: <span className="user-name">{user.displayName}</span></span>
                     <span>|</span>
@@ -23,7 +24,7 @@ export function AuthArea() {
                     </button>
                 </>
             }
-            {!user &&
+            {!user && !loading &&
                 <NavLink
                     to="auth"
                     tabIndex={0}
